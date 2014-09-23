4e4a118c4c441c41cbe50f6f0a2991690ed1bc5c
Jesse Luehrs <doy@tozt.net>
2014-09-23 00:01:17 -0400

    allow creating distributable binaries

--- setup.py
+++ setup.py
@@ -1,4 +1,28 @@
+import sys
+
 from setuptools import setup, find_packages
+
+if len(sys.argv) > 1 and sys.argv[1] == "fatpack":
+    import glob
+    import io
+    import zipfile
+
+    zipdata = io.BytesIO()
+    z = zipfile.ZipFile(zipdata, mode="w", compression=zipfile.ZIP_DEFLATED)
+    for filename in glob.iglob("termcast_client/*.py"):
+        if filename[-11:] == "__main__.py":
+            arcname = "__main__.py"
+        else:
+            arcname = filename
+        with open(filename, "rb") as f:
+            z.write(filename, arcname=arcname)
+    z.close()
+
+    with open("termcast", "wb") as f:
+        f.write(b"#!/usr/bin/env python\n")
+        f.write(zipdata.getvalue())
+
+    sys.exit()
 
 setup(
     name="termcast_client",
